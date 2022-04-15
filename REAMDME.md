af1d5c69bb99dcac3729c86d87d01ec863fa9371
marcus-hugo <78324205+marcus-hugo@users.noreply.github.com>
2022-04-15 13:15:46 -0500

    initial commit

---
+++ REAMDME.md
@@ -0,0 +1,10 @@
+# Scrimba Solo Project - Unit Converter
+
+## About
+Scrimba Learn JavaScript for Free Solo Project built from a Figma design.  The requirements are to save a number as a variable, on load make conversion calculations.  Numbers must be rounded to 3 decimal places. 
+
+💪Stretch goal: to add an input for the user.💪
+
+### Notes
+
+### Resources
---
+++ app.js
@@ -0,0 +1,44 @@
+
+const numInput = document.getElementById('number-input');
+const displayLengthEl = document.getElementById('display-length-element');
+const displayVolumeEl = document.getElementById('display-volume-element');
+const displayMassEl = document.getElementById('display-mass-element');
+
+numInput.addEventListener('input', function() {
+    numInputValue = parseFloat(numInput.value).toFixed(3);
+    console.log(numInputValue)
+
+    //////////////// LENGTH /////////////////////////////////////
+    // meters to feet (formula = meter * 3.281 foot)
+    let metersToFeet = (numInputValue * 3.281).toFixed(3);
+    //  feet to meters(formula = foot / 3.281 foot)
+    let feetToMeters = (numInputValue / 3.281).toFixed(3);
+    // display Length
+    displayLengthEl.textContent = `
+        ${numInputValue} meters = ${metersToFeet} feet | 
+        ${numInputValue} feet = ${feetToMeters} meters
+        `;
+
+    //////////////// VOLUME /////////////////////////////////////
+    // liters to gallons (formula = liter / 4.546)
+    let litersToGallons = (numInputValue / 4.546).toFixed(3);
+    // gallons to liters (formula = gallon * 4.546)
+    let gallonsToLiters = (numInputValue * 4.546).toFixed(3);
+    // display Volume
+    displayVolumeEl.textContent = `
+        ${numInputValue} liters = ${litersToGallons} gallons | 
+        ${numInputValue} gallons = ${gallonsToLiters} liters
+        `;
+
+    //////////////// MASS /////////////////////////////////////
+
+    // kilos to pounds (formula = kilo * 2.205)
+    let kilosToPounds = (numInputValue * 2.205 ).toFixed(3);
+    // pounds to kilos (formula = kilo / 2.205)
+    let PoundsToKilos  = (numInputValue / 2.205).toFixed(3);
+
+    displayMassEl.textContent = `
+    ${numInputValue} kilos = ${litersToGallons} pounds | 
+    ${numInputValue} Pounds = ${gallonsToLiters} kilos
+    `;
+})
